4f2db171428d99bac435f80bb5e42e9899c5bdbd
Morgan Holve <holve100@mail.chapman.edu>
2018-10-13 17:34:52 +0000

    Modified module

--- cw07.py
+++ cw07.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 ###
-# Name: YOUR_FULL_NAME_HERE
-# Student ID: ID_HERE
-# Email: CHAPMAN_EMAIL_HERE
-# Course: PHYS220/MATH220/CPSC220 Fall 2018
-# Assignment: HOMEWORK_OR_CLASSWORK_NUMBER
+# Name: Morgan Holve
+# Student ID: 2281337
+# Email: holve100@mail.chapman.edu
+# Course: MATH220 Fall 2018
+# Assignment: CW 7
 ###
 
 """Classwork 07
@@ -45,11 +45,7 @@ def gen_gaussian_list(a, b, n=1000):
 
 def gen_gaussian_array(a, b, n=1000):
     """gen_gaussian_array(a, b, n=1000)
-    Generate a discrete approximation of a Gaussian function, including its
-    domain and range, stored as a pair of numpy arrays.
-    
-    Args:
-        a (float) : Lower bound of domain
+    Generate a discrete approximation of a Gaussian function, including it
         b (float) : Upper bound of domain
         n (int, optional) : Number of points in domain, defaults to 1000.
     
@@ -58,9 +54,83 @@ def gen_gaussian_array(a, b, n=1000):
             x  : [a, ..., b] Array of n equally spaced float64 between a and b
             g  : [g(a), ..., g(b)] Array of Gaussian values matched to x
     """
-    pass
+    x = np.linspace(a, b, num=n, endpoint=True)
+    g = (1/np.sqrt(2*np.pi))*np.exp(-x**2/2)
+    return (x, g)
 
+def gen_sinc_list(a,b,n=1000):
+    """
+    Generate a discrete approximation of a Sinc function
+    
+    Args:
+        a (float) : Lower bound of domain
+        b (float) : Upper bound of domain
+        n (int, optional) : Number of points in domain, defaults to 1000.
+    
+    Returns:
+        (x, s) : Pair of lists of floats
+            x  : [a, ..., b] List of n equally spaced floats between a and b
+            s  : [s(a), ..., s(b)] List of Sinc(x) values
+    """
+    dx = (b-a)/(n-1)
+    x = [a + k*dx for k in range(n)]
+    def sin(x):
+        return (math.sin(x)/x)
+    s = [sin(i) for i in x]
+    return (x, s)
+    
+def gen_sinc_array(a,b,n=1000):
+    """
+    Generate a discrete approximation of a Sinc function
+        b (float) : Upper bound of domain
+        n (int, optional) : Number of points in domain, defaults to 1000.
+    
+    Returns:
+        (x, s) : Pair of numpy arrays of float64
+            x  : [a, ..., b] Array of n equally spaced float64 between a and b
+            s : [s(a), ..., s(b)] Array of Sinc(x) values
+    """
+    x = np.linspace(a, b, num=n, endpoint=True)
+    s = np.ones_like(x)
+    np.divide(np.sin(x), x, out=s, where=x!=0)
+    return(x,s)
 
+def gen_sinf_list(a,b,n=1000):
+    """
+    Generate a discrete approximation of a Sinf function
+    
+    Args:
+        a (float) : Lower bound of domain
+        b (float) : Upper bound of domain
+        n (int, optional) : Number of points in domain, defaults to 1000.
+    
+    Returns:
+        (x, s2) : Pair of lists of floats
+            x  : [a, ..., b] List of n equally spaced floats between a and b
+            s2  : [s2(a), ..., s2(b)] List of Sinf(x) values
+    """
+    dx = (b-a)/(n-1)
+    x = [a + k*dx for k in range(1,n)]
+    def sin(x):
+        return (math.sin((1/x)))
+    s2 = [sin(i) for i in x]
+    return (x, s2)
+
+def gen_sinf_array(a,b,n=1000):
+    """
+    Generate a discrete approximation of a Sinf function
+        b (float) : Upper bound of domain
+        n (int, optional) : Number of points in domain, defaults to 1000.
+    
+    Returns:
+        (x, s2) : Pair of numpy arrays of float64
+            x  : [a, ..., b] Array of n equally spaced float64 between a and b
+            s2 : [s2(a), ..., s2(b)] Array of Sinf(x) values
+    """
+    x = np.linspace(a, b, num=n, endpoint=True)
+    s2 = np.ones_like(x)
+    np.divide(np.sin(1/x), 1, out=s2, where=x!=0)
+    return(x,s2)
 def main(a,b,n=1000):
     """main(a, b, n=1000)
     Main function for command line operation. Prints result of Gaussian to screen.
